59f98caa8671e445ea6dfb79b6bdb81745319775
slogan-punz <slogan@cat.pdx.edu>
2016-01-21 04:05:39 -0800

    jokebot

--- command.sh
+++ command.sh
@@ -5,9 +5,7 @@ read nick buff word
 key="$(cat notakey.txt)"
 function call { $(echo "$1" |grep -P "$2" > /dev/null) ; }
 function output { echo "PRIVMSG $1 :$2" ; }
-
-
-if   call "$word" "\bjokebot: joke\b"\|"\!joke" ;  then
+function tellJoke {    
     batchNum=$(sed -n "1p" counter.txt)
     jokeNum="$(wc -l < jokes.txt)"
     (( batches = $jokeNum / 3 ))
@@ -32,7 +30,62 @@ if   call "$word" "\bjokebot: joke\b"\|"\!joke" ;  then
     output $buff "$nick: $lineNum: $joke"
     output $buff "$nick: $lineNum: $awns"
 
-elif call "$word" "\bjokebot: newJoke\b" ; then
+    }
+
+if   call "$word" "\bjokebot: joke\b"\|"\!joke" ;  then
+   # batchNum=$(sed -n "1p" counter.txt)
+   # jokeNum="$(wc -l < jokes.txt)"
+   # (( batches = $jokeNum / 3 ))
+   # >counter.txt
+   # let "batchNum++"
+   # if [ $batchNum == $batches ]; then
+   #     batchNum=0
+   #     echo "$batchNum" >> counter.txt
+   # else
+   #     echo "$batchNum" >> counter.txt
+   # fi
+   # 
+   # batchOne=$(($RANDOM%3))
+   # batchTen=$((3 * $batchNum))
+
+   # lineNum=$(($batchTen + $batchOne))
+   # let "lineNum++"
+
+   # joke=$(sed -n "${lineNum}p" jokes.txt)
+   # awns=$(sed -n "${lineNum}p" awnsers.txt)
+    
+   # output $buff "$nick: $lineNum: $joke"
+   # output $buff "$nick: $lineNum: $awns"
+
+
+   buffmsg="$(echo $word | cut -d "#" -f2)"
+   msg="$(echo $buffmsg | cut -d ":" -f 2-3)"
+   num1="$(echo $msg | cut -d "e" -f3)"
+   num2="$(echo $msg | cut -d "e" -f2)"
+
+    if [[ $num1 = *[[:digit:]]* ]] ; then
+        joke=$(sed -n "${num1}p" jokes.txt)
+        awns=$(sed -n "${num1}p" awnsers.txt)
+
+        output $buff "$nick: $num1: $joke"
+        output $buff "$nick: $num1: $awns"
+
+    elif [[ $num2 = *[[:digit:]]* ]] ; then
+         joke=$(sed -n "${num2}p" jokes.txt)
+         awns=$(sed -n "${num2}p" awnsers.txt)
+         output $buff "$nick: $num2: $joke"
+         output $buff "$nick: $num2: $awns"
+
+    else
+        tellJoke
+    fi
+
+    
+     
+
+
+   # tellJoke
+elif call "$word" "\bjokebot: newJoke\b"\|"\bjokebot: newjoke\b"\|"\bjokebot: addjoke\b"\|"\bjokebot: addJoke\b" ; then
     jokeLine="$(wc -l < newJoke.txt)"
     punchLine="$(wc -l < newPunch.txt)"
 
@@ -49,6 +102,7 @@ elif call "$word" "\bjokebot: newJoke\b" ; then
             output $buff "$joke"
             output $buff "$punch"
             output $buff "$nick: This is how your joke will look, if this isn't correct please, use \"oops\" to remove that joke (It really just removes the last inputted joke so be careful)"
+            output "punz" "$joke/$punch"
             echo $joke>>newJoke.txt
             echo $punch>>newPunch.txt
 
@@ -61,7 +115,7 @@ elif call "$word" "\bjokebot: newJoke\b" ; then
                 cat newPunch.txt>>awnsers.txt
                 cat newJoke.txt>>jokes2.txt
                 catnewJoke.txt>>awnsers2.txt
-                jokeNum="$(wc -l < newJoke.txt)"
+                jokeNum="$(wc -l < jokes.txt)"
                 output $buff "NEW JOKES :D (last joke was $jokeNum)"
                 >newJoke.txt
                 >newPunch.txt
@@ -76,28 +130,8 @@ elif call "$word" "\bjokebot: newJoke\b" ; then
         output $buff "$nick: Sorry seems something is wrong right now and I'm not taking jokes, I'll PM punz to fix me."
         output "punz" "temporary joke files have different line counts"
     fi
-    
-#    jokeLine="$(wc -l < newJoke.txt)"
-#    echo $jokeLine
-#    if [ $jokeLine==10 ] ; then 
-#         cat newJoke.txt>>temp.txt
-#         cat newPunch.txt>>temp2.txt
-#      
-#         output $buff "NEW JOKES :D"
-#         >newJoke.txt
-#         >newPunch.txt
-#    fi
 
-
-    #elif call "$word" "\bjokebot: newpunchline\b" ; then
-    #   cut1="$(echo $word | cut -f2 -d "#" )"
-    #  newpunch="$(echo $cut1 | cut -d " " -f 4-1000)"
-    # output $buff "$newpunch"
-    # jokeline=$(wc -l newjoke.txt 
-    # echo $newpunch>>newpunch.txt
-    
-
-elif call "$word" "\bjokebot: oops\b" ; then
+elif call "$word" "\bjokebot: oops\b"\|"\bjokebot: remove\b" ; then
     lines="$(wc -l < newJoke.txt)"
     #let "lines++"
     joke=$(sed -n "${lines}p" newJoke.txt)
@@ -106,7 +140,7 @@ elif call "$word" "\bjokebot: oops\b" ; then
     sed -i "${lines} d" newPunch.txt
     output $buff "$nick: \"$joke/$punch\" has been removed"
 
-elif call "$word" "\bjokebot: purge\b" ; then
+elif call "$word" "\bjokebot: purge\b"\|"\bjokebot: Purge\b" ; then
     cut1="$(echo $word | cut -d "#" -f2)"
     num="$(echo $cut1 | cut -d " " -f 4)"
     joke=$(sed -n "${num}p" jokes.txt)
@@ -129,7 +163,7 @@ elif call "$word" "\bjokebot: hi\b"\|"jokebot: Hi" ; then
      output $buff "Hi $nick"
 
 
-elif call "$word" "\bjokebot: info\b" ; then
+elif call "$word" "\bjokebot: info\b"\|"\bjokebot: help\b"\|"\bjokebot: Help\b"\|"\bjokebot: Info\b" ; then
     output $buff "Hi I'm jokebot! I tell jokes(good or bad is up to you). My main command is \"joke\" but you can also use \"!joke\", I'll only listen if your talking to me(through jokebot: <command>). For a list of commands use \"commands\". To add a joke use \"jokehelp\". I'm a work in progress so if you have any questions, PM punz."
 
 
@@ -150,6 +184,7 @@ elif call "$word" "\bjokebot: privmsg\b" ; then
     output $nick "hi" 
 
 elif call "$nick" "\bpunz\b" && call "$word" "jokebot: testing" ; then
+    output "#robots" "Not broken just testing"
     echo "PART #dadjokes"
     echo "PART #robots"
 
