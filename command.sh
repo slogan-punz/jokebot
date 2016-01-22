c8927d31bac1804bf915ed748b85dae90fc8a2dc
slogan-punz <slogan@cat.pdx.edu>
2016-01-22 04:26:42 -0800

    jokebot

--- command.sh
+++ command.sh
@@ -5,6 +5,56 @@ read nick buff word
 key="$(cat notakey.txt)"
 function call { $(echo "$1" |grep -P "$2" > /dev/null) ; }
 function output { echo "PRIVMSG $1 :$2" ; }
+function nameTestAdd {
+    ##This function is used to test if the user is in the name text file
+    ##If the users name is in the text file it will first store the liine the user is on
+    ##See how many jokes are in the jokefile
+    ##the line the user name and last joke number is on
+    ##append with username and new jokenum
+    ##if user is not found just append with username and new joke num
+
+    if grep -q "$nick" names.txt ; then
+        lineNum="$(grep -n "$nick" names.txt | cut -f1 -d:)"
+        #jokeNum=$(sed -n "${lineNum}p" names.txt | cut -f2 -d:)
+        jokeNum=$(wc -l < jokes.txt)
+        sed -i "${lineNum} d" names.txt
+        echo "$nick:$jokeNum">>names.txt
+
+    else
+        jokeTot=$(wc -l < jokes.txt)
+        echo "$nick:$jokeTot">>names.txt
+
+    fi
+    
+}
+function oops {
+    ##this function is used to remove the last joke inputted by a user
+    ##It checks a file for the users name goes from there
+    ##If there it takes the line that it is located. takes the text from that line, and cuts that line for the number
+    ##this number is used to remove and output what the removed joke was
+    ##the user and their number is then removed from the file
+    ## if the user is not inn the file the program only oupts a simple phrase
+
+
+    if grep -q "$nick" names.txt ; then
+        lineNum="$(grep -n "$nick" names.txt | cut -f1 -d:)"
+        echo "lineNum $lineNum"
+        jokeNum=$(sed -n "${lineNum}p" names.txt | cut -f2 -d:)
+
+        joke=$(sed -n "${jokeNum}p" jokes.txt)
+        punch=$(sed -n "${jokeNum}p" awnsers.txt)
+
+        sed -i "${jokeNum} d" jokes.txt
+        sed -i "${jokeNum} d" awnsers.txt
+        sed -i "${lineNum} d" names.txt
+        output $buff "$nick: Your joke \"$joke/$punch\" has been removed"
+
+
+    else 
+        output $buff "You removed your last joke already"
+    fi
+    
+}
 function tellJoke {
     ## This this system makes sure that the same joke is never told twice
     ## It also is semi random
@@ -46,6 +96,7 @@ function tellJoke {
     fi
 
     }
+#function addJoke {}
 
 if   call "$word" "\bjokebot: joke\b"\|"\!joke" ;  then
    
@@ -83,41 +134,46 @@ if   call "$word" "\bjokebot: joke\b"\|"\!joke" ;  then
 elif call "$word" "\bjokebot: newJoke\b"\|"\bjokebot: newjoke\b"\|"\bjokebot: addjoke\b"\|"\bjokebot: addJoke\b" ; then
     
     ## This allows for users to add their own jokes    
+
     
+        
     jokeLine="$(wc -l < newJoke.txt)"
     punchLine="$(wc -l < newPunch.txt)"
-
     if [ $jokeLine == $punchLine ] ; then    
-
-
+        
+        
+        
         cut1="$(echo $word | cut -d "#" -f2)"
         if $(echo $cut1 | grep "/" > /dev/null) ; then
             punch="$(echo $cut1 | cut -d "/" -f2)"
             cut2="$(echo $cut1 | cut -d "/" -f1)"
             joke="$(echo $cut2 | cut -d " " -f 4-100)"
 
-        
+            
+            echo $joke>>jokes.txt
+            echo $punch>>awnsers.txt
+            jokeLine="$(wc -l < jokes.txt)"
             output $buff "$joke"
             output $buff "$punch"
-            output $buff "$nick: This is how your joke will look, if this isn't correct please, use \"oops\" to remove that joke (It really just removes the last inputted joke so be careful)"
+            output $buff "$nick: your joke is number $jokeLine, use \"oops\" to remove your last inputted joke"
             output "punz" "$joke/$punch"
-            echo $joke>>newJoke.txt
-            echo $punch>>newPunch.txt
-
-            jokeLine="$(wc -l < newJoke.txt)"
-            num=3
-            echo $jokeLine
-            echo $num
-            if [ $jokeLine == $num ] ; then
-                cat newJoke.txt>>jokes.txt
-                cat newPunch.txt>>awnsers.txt
-                cat newJoke.txt>>jokes2.txt
-                catnewJoke.txt>>awnsers2.txt
-                jokeNum="$(wc -l < jokes.txt)"
-                output $buff "NEW JOKES :D (last joke was $jokeNum)"
-                >newJoke.txt
-                >newPunch.txt
-            fi
+            #echo $joke>>newJoke.txt
+            #echo $punch>>newPunch.txt
+            nameTestAdd
+            #jokeLine="$(wc -l < newJoke.txt)"
+            #num=3
+            #echo $jokeLine
+            #echo $num
+            #if [ $jokeLine == $num ] ; then
+            #    cat newJoke.txt>>jokes.txt
+            #    cat newPunch.txt>>awnsers.txt
+            #    cat newJoke.txt>>jokes2.txt
+            #    catnewJoke.txt>>awnsers2.txt
+            #    jokeNum="$(wc -l < jokes.txt)"
+            #    output $buff "NEW JOKES :D (last joke was $jokeNum)"
+            #    >newJoke.txt
+            #    >newPunch.txt
+            #fi
 
 
 
@@ -130,13 +186,7 @@ elif call "$word" "\bjokebot: newJoke\b"\|"\bjokebot: newjoke\b"\|"\bjokebot: ad
     fi
 
 elif call "$word" "\bjokebot: oops\b"\|"\bjokebot: remove\b" ; then
-    lines="$(wc -l < newJoke.txt)"
-    #let "lines++"
-    joke=$(sed -n "${lines}p" newJoke.txt)
-    punch=$(sed -n "${lines}p" newPunch.txt)
-    sed -i "${lines} d" newJoke.txt
-    sed -i "${lines} d" newPunch.txt
-    output $buff "$nick: \"$joke/$punch\" has been removed"
+    oops
 
 elif call "$word" "\bjokebot: purge\b"\|"\bjokebot: Purge\b" ; then
     cut1="$(echo $word | cut -d "#" -f2)"
@@ -149,9 +199,9 @@ elif call "$word" "\bjokebot: purge\b"\|"\bjokebot: Purge\b" ; then
     output "punz" "$nick has removed \"$num:$joke:$punch\""
 
 elif call "$word" "\bjokebot: jokehelp\b" ; then
-    output $buff "To add a joke you must use the command newJoke, followed by your joke divided by a /"
+    output $buff "Use the command \"newJoke\" followed by your joke divided by a \"/\" to add a joke."
     output $buff "example \" jokebot: newJoke setup/punchline \", if you mess up just say jokebot: oops"
-    output $buff "to remove a joke use purge followed by the number of the joke"
+    output $buff "Use jokebot: purge followed by a number to remove a specific joke"
     output $buff "example \"jokebot: purge 91\""
     
 elif call "$word" "\bjokebot: ba dum\b" ; then
@@ -166,7 +216,7 @@ elif call "$word" "\bjokebot: info\b"\|"\bjokebot: help\b"\|"\bjokebot: Help\b"\
 
 
 elif call "$word" "\bjokebot: commands\b"\|"jokebot: command" ; then
-    output $buff "$nick: joke, Hi, info, commands, jokenum, bad one, good one, jokehelp, newJoke, purge, oops. You can also just use "!joke" to get the same affect as joke"
+    output $buff "$nick: joke, Hi, info, commands, source, jokenum, bad one, good one, jokehelp, newJoke, purge, oops. You can also just use "!joke" to get the same affect as joke"
 
 elif call "$word" "\bjokebot: jokenum\b" ; then
     jokeNum="$(wc -l < jokes.txt)"
